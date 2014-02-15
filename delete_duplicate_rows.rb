3621552907b7cb9cb35a2bb538561761beaf5c41
矢口 貴彦 <yaguchitakahiko@yaguchi-no-MacBook-Air.local>
2014-02-15 22:35:10 +0900

    mod

--- delete_duplicate_rows.rb
+++ delete_duplicate_rows.rb
@@ -2,11 +2,11 @@ require "csv"
 
 buf_row0 = "0";                                                                  
 CSV.open("story_title_master_q.csv", "w") do |csv|
-	CSV.foreach("story_title_master.csv", "r") do |row|
-		unless row[0] == buf_row0
-			csv << row
-		end
-		buf_row0 = row[0]
+    CSV.foreach("story_title_master.csv", "r") do |row|
+	unless row[0] == buf_row0
+	    csv << row
 	end
+	buf_row0 = row[0]
+    end
 end
 
