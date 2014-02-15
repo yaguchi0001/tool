793a7240ec169ad6196def0c937eace27b7bf343
矢口 貴彦 <yaguchitakahiko@yaguchi-no-MacBook-Air.local>
2014-02-15 20:35:45 +0900

    first commit

---
+++ delete_duplicate_rows.rb
@@ -0,0 +1,12 @@
+require "csv"
+
+buf_row0 = "0";                                                                  
+CSV.open("story_title_master_q.csv", "w") do |csv|
+	CSV.foreach("story_title_master.csv", "r") do |row|
+		unless row[0] == buf_row0
+			csv << row
+		end
+		buf_row0 = row[0]
+	end
+end
+
